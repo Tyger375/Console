a8e2a46034e30e382ee0b9fe5874f648e23bd363
Tyger375 <edoardo.takanen@gmail.com>
2025-03-27 20:11:22 +0100

    commit

--- ROM Programmer/main.py
+++ ROM Programmer/main.py
@@ -11,21 +11,30 @@ if __name__ == "__main__":
         d = op[0]
         b = bytearray([0x00])
         if d == "read":
-            if len(op) != 3:
+            if len(op) < 3:
                 continue
             first = int(op[1], 16)
             second = int(op[2], 16)
             b = bytearray([0x10, first, second])
             s.write(b)
         elif d == "write":
-            if len(op) != 4:
+            if len(op) < 4:
                 continue
             b = bytearray([0x20, int(op[1], 16), int(op[2], 16), int(op[3], 16)])
             s.write(b)
         elif d == "load":
-            b = bytearray([0x30, 0x00, 0x09])
-            s.write(b)
-            s.write(bytearray([0xAF, 0x21, 0x00, 0xF0, 0x3C, 0x77, 0x18, 0xFC, 0x00]))
+            if len(op) < 2:
+                continue
+            filename = op[1]
+            with open(filename, "rb") as f:
+                data = f.read()
+                length = len(data)
+                if length > (8 * 1024):
+                    print("file is too big")
+                    continue
+                b = bytearray([0x30, length & (0xFF00), length & 0xFF])
+                s.write(b)
+                s.write(data)
 
         str = s.readline()
         v = str.strip()
